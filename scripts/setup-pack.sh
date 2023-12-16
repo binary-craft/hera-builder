bd36de1d5a10b7caed7190b6645ad12ef0ffd34e
Pim Huisman <44083489+pim-huisman@users.noreply.github.com>
2023-12-16 12:21:09 +0100

    feat: multiplatform builds (#2)
    * feat: support multiplatform by building with a matrix
    
    * feat: separate script to properly install Pack multiplatform
    
    * feat: added a build-and-push workflow for main branch, simplified the regular build
    
    ---------
    
    Co-authored-by: Pim Huisman <pim-huisman@users.noreply.github.com>

---
+++ scripts/setup-pack.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Custom script to install pack. Needed because buildpacks/github-actions/setup-pack does not properly support anything other than Linux.
+
+set -euo pipefail
+
+mkdir -p "${HOME}"/bin
+echo "PATH=${HOME}/bin:${PATH}" >> "${GITHUB_ENV}"
+
+if [ "$RUNNER_OS" == "macOS" ]; then
+   PLATFORM="macos"
+elif [ "$RUNNER_OS" == "Windows" ]; then
+   PLATFORM="windows"
+else
+   PLATFORM="linux"
+fi
+if [ "$RUNNER_ARCH" == "ARM64" ]; then
+   PLATFORM="${PLATFORM}-arm64"
+fi
+
+PACK_VERSION="0.32.1"
+DOWNLOAD_URL="https://github.com/buildpacks/pack/releases/download/v${PACK_VERSION}/pack-v${PACK_VERSION}-${PLATFORM}.tgz"
+echo "Installing pack ${PACK_VERSION} from ${DOWNLOAD_URL}"
+curl \
+  --show-error \
+  --silent \
+  --location \
+  --fail \
+  --retry 3 \
+  --connect-timeout 5 \
+  --max-time 60 \
+  "${DOWNLOAD_URL}" \
+| tar -C "${HOME}/bin" -xz pack
+
+if [[ ! -f "${HOME}/bin/pack" ]]; then
+   echo "Failed to install pack! Could not be found at ${HOME}/bin/pack"
+   exit 1
+fi
+
+if [[ ! -x "${HOME}/bin/pack" ]]; then
+   echo "Failed to install pack! Pack at ${HOME}/bin/pack is not executable"
+   exit 2
+fi
